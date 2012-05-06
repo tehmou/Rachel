ae2bdfcbd18ee4c7886b551ece315e98c3c32960
Timo Tuominen <timo.tuominen@gmail.com>
2012-05-06 13:56:35 +0200

    Change expected dataType to xml.

--- js/code.js
+++ js/code.js
@@ -37,16 +37,18 @@ $(function () {
     }
 
     $.ajax({
-       url: "data.kml",
-       success: function (data) {
-           $.ajax({
-               url: "network.kml",
-               success: function (network) {
-                   map = createMap(data, network);
-                   initialize();
-               }
-           });
-       }
+        url: "data.kml",
+        dataType: "xml",
+        success: function (data) {
+            $.ajax({
+                url: "network.kml",
+                dataType: "xml",
+                success: function (network) {
+                       map = createMap(data, network);
+                       initialize();
+                }
+            });
+        }
     });
 
 });
